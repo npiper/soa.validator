3f49e332d97d3f766eb15fa907cca5134922d278
Neil Piper <n_piper@hotmail.com>
2015-03-25 23:45:07 +1100

    Updated with working test case examples & dependencies in POM's

---
+++ soa.rules/src/test/java/net/sourceforge/pmd/lang/xml/rule/ComplexTypeNamingConventionTest.java
@@ -0,0 +1,20 @@
+package net.sourceforge.pmd.lang.xml.rule;
+
+import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import org.junit.Before;
+
+public class ComplexTypeNamingConventionTest extends SimpleAggregatorTst {
+
+	private static final String RULESET = "xml-basic";
+
+    @Before
+    public void setUp() {
+        addRule(RULESET, "ComplexTypeElementNamingConvention");
+    }
+
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(ComplexTypeNamingConventionTest.class);
+    }
+
+	
+}
